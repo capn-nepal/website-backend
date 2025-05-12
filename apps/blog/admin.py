b8f714e0e5e0f44e0727fa252fa4f127387658b2
sudip-khanal <sudipkhanal0777@gmail.com>
2025-05-12 13:23:06 +0545

    feat(api): reference author with user

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Blog
+
+from .models import Blog, BlogAsset
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date', 'featured')
-    search_fields = ('title', 'author')
-    list_filter = ('published_date', 'featured')
+    list_display = ("title", "author", "published_date", "featured")
+    search_fields = ("title", "author")
+    list_filter = ("published_date", "featured")
+
+
+@admin.register(BlogAsset)
+class BlogAssetsAdmin(admin.ModelAdmin):
+    list_display = ("id", "blog")
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,26 +1,33 @@
 from django.db import models
-from apps.common.models import UserResource
 from django.utils.translation import gettext_lazy as _
+
+from apps.common.models import UserResource
+from apps.user.models import User
 
 
 class Blog(UserResource):
-    title = models.CharField(max_length=232, verbose_name=_('Title'))
-    published_date = models.DateField(verbose_name=_('Published Date'))
-    author = models.CharField(max_length=232, verbose_name=_('Author'))
-    content = models.TextField(verbose_name=_('Content'))
-    description = models.CharField(verbose_name=_('Description'))
-    cover_image = models.ImageField(
-        upload_to='blogs/',
-        verbose_name=_('Cover Image')
+    title = models.CharField(max_length=232, verbose_name=_("Title"))
+    published_date = models.DateField(verbose_name=_("Published Date"))
+    author = models.ForeignKey(
+        User,
+        related_name="blogs",
+        on_delete=models.PROTECT,
+        verbose_name=_("Author"),
     )
-    featured = models.BooleanField(verbose_name=_('Featured'), default=False)
+    content = models.TextField(verbose_name=_("Content"))
+    description = models.CharField(verbose_name=_("Description"))
+    cover_image = models.ImageField(
+        upload_to="blogs/",
+        verbose_name=_("Cover Image"),
+    )
+    featured = models.BooleanField(verbose_name=_("Featured"), default=False)
 
     def __str__(self):
         return self.title
 
 
 class BlogAsset(UserResource):
-    file = models.FileField(verbose_name=_('File'))
+    file = models.FileField(verbose_name=_("File"))
     blog = models.ForeignKey(
         Blog,
         related_name="%(class)s_file",
