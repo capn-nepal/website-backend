9019ab972b1e590d6a97b8d7f67dd9a03cd90fac
sudip-khanal <sudipkhanal0777@gmail.com>
2025-05-17 13:16:40 +0545

    feat(api): add change password mutation

--- apps/user/graphql/inputs.py
+++ apps/user/graphql/inputs.py
@@ -8,3 +8,9 @@ from apps.user.models import User
 class LoginInput:
     email: strawberry.auto
     password: strawberry.auto
+
+
+@strawberry.input
+class ChangePasswordInput:
+    old_password: str
+    new_password: str
--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,6 +1,7 @@
 import typing
 
 from django.contrib.auth import authenticate
+from django.utils.translation import gettext
 from rest_framework import serializers
 
 from .validators import CustomMaximumLengthValidator
@@ -25,3 +26,30 @@ class LoginSerializer(serializers.Serializer):
         if authenticate_user is None:
             raise serializers.ValidationError("No active account found with the given credentials")
         return {"user": authenticate_user}
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, password):
+        user = self.context["request"].user
+        if not user.check_password(password):
+            raise serializers.ValidationError(gettext("Invalid old Password"))
+        return password
+
+    def validate(self, attrs):
+        if attrs["old_password"] == attrs["new_password"]:
+            raise serializers.ValidationError(gettext("New and old provided passwords are same"))
+        return attrs
+
+    def validate_new_password(self, password):
+        CustomMaximumLengthValidator().validate(password=password)
+        return password
+
+    def save(self, **_):
+        assert isinstance(self.validated_data, dict)
+        user = self.context["request"].user
+        new_password = self.validated_data["new_password"]
+        user.set_password(new_password)
+        user.save(update_fields=("password",))
